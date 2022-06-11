0d997776bab26387218f9c1ccd25f6e2767ddff4
svenpetermanec <sven.petermanec@gmail.com>
2022-06-11 18:20:02 +0200

    Install redux and react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
+  <Provider store={store}>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+  </Provider>
 );
